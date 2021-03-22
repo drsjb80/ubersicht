800906a6c367f07f4c90c616ee8d8fd476d11c1a
drsjb80 <drsjb80@gmail.com>
2021-03-22 13:22:13 -0600

    initial

---
+++ dilbert.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+cd ~/Library/Application\ Support/Übersicht/widgets
+rm -f dilbert.gif
+
+date=$(date +"%Y-%m-%d")
+image=$(curl -s https://dilbert.com/strip/"${date}" | \
+	 grep data-image | \
+	 sed -e 's/^.*data-image="//' -e 's/".*//')
+curl -s -o /tmp/$$ "${image}"
+/usr/local/bin/convert /tmp/$$ -resize 500 dilbert.gif
